f44448d535241b39accc015338bf25ad37e03f34
somnathraz <somnathkhadanga810@gmail.com>
2022-09-06 19:10:16 +0530

    cookie change

--- pages/member/auth/dashboard.js
+++ pages/member/auth/dashboard.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import cookies from "next-cookies";
+import { useRouter } from "next/router";
 
-const Dashboard = () => {
+const Dashboard = ({ initialName }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (initialName === "") {
+      router.push("/member/auth");
+    }
+  });
+
   return (
     <div>
-      <h2>Welcome</h2>
+      <h2>Welcome {initialName.replace("@skillslash.com", "")}</h2>
     </div>
   );
 };
 
 export default Dashboard;
-// Dashboard.getInitialProps = async (ctx) => {
-//   return {
-//     initialName: cookies(ctx).USER || "",
-//   };
-// };
+Dashboard.getInitialProps = async (ctx) => {
+  return {
+    initialName: cookies(ctx).USER || "",
+  };
+};
