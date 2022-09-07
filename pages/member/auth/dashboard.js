1614d8393f76f35e4f374ad1fe086ba4c03fd480
somnathraz <somnathkhadanga810@gmail.com>
2022-09-07 08:19:30 +0530

    banner change

--- pages/member/auth/dashboard.js
+++ pages/member/auth/dashboard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { withAuthSync } from "../../../lib/auth";
 
 import nextCookie from "next-cookies";
 
@@ -11,7 +10,7 @@ const Dashboard = ({ initialName, token }) => {
   );
 };
 
-export default withAuthSync(Dashboard);
+export default Dashboard;
 Dashboard.getInitialProps = async (ctx) => {
   const { token, USER } = nextCookie(ctx);
   return {
