ee49d7e7a9e674f1d006481844e60fe919d8a15a
somnath khadanga <somnathkhadanga@somnaths-MacBook-Pro.local>
2023-12-01 14:10:15 +0530

    changes

--- pages/api/v1/generateCertificate.js
+++ pages/api/v1/generateCertificate.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
       vertical,
       point,
     } = req.body;
-    console.log(req.body);
+
     let path = "";
     if (certificateType === "course completion certificate") {
       path = "./certificate/courseCompletion.html";
